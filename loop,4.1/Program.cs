9bd3ec1afa1fd4397df7bcc6ccf650ac65f8d490
muja07001 <muja07001@E-PW05CGR9>
2024-11-26 14:12:50 +0100

    Add project files.

---
+++ loop,4.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loop_4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vilket är världens folkrikaste LAND?");
+            string svar = Console.ReadLine();
+            while (svar != "kina")
+            {
+                Console.WriteLine("du svarade fel, försök igen");
+                svar = Console.ReadLine();
+
+            }
+            Console.WriteLine("grattis! du svarade rätt");
+        }
+    }
+}
